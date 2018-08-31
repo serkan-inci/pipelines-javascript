8795bdfff5ee5a997d08784429e2bfef0173edc0
Serkan Inci <serkan.inci@hotmail.com>
2018-08-31 15:19:39 -0400

    Adding math tests

--- tests/math_test.js
+++ tests/math_test.js
@@ -1,1 +1,9 @@
+describe('Math', function () {
+    describe('#floor', function () {
+        it('basic', function () {
+            var myNumber = 5.7;
 
+            assert.equal(6, Math.floor(myNumber));
+        });
+    });
+});
