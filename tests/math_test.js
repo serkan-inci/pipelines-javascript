cf9ec83841c9bee577ab42936210acc00d9dd191
Serkan Inci <serkan.inci@hotmail.com>
2018-08-31 15:21:06 -0400

    Deleting failing test

--- tests/math_test.js
+++
@@ -1,9 +0,0 @@
-describe('Math', function () {
-    describe('#floor', function () {
-        it('basic', function () {
-            var myNumber = 5.7;
-
-            assert.equal(6, Math.floor(myNumber));
-        });
-    });
-});
